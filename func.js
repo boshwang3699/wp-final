4c60b0a4d3a784b194c5c73f71a0e66ca31e454c
pohsunwang <96949018+boshwang3699@users.noreply.github.com>
2022-06-06 15:30:07 +0800

    Add files via upload

---
+++ func.js
@@ -0,0 +1,86 @@
+function moreSkills(b){
+    document.getElementById("tbl").innerHTML = `
+    <tr>
+        <td>HTML</td>
+        <td>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+        </td>
+    </tr>
+    <tr>
+        <td>JavaScript</td>
+        <td>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+        </td>
+    </tr>
+    <tr>
+        <td>PHP</td>
+        <td>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+        </td>
+    </tr>
+    <tr>
+        <td>Java</td>
+        <td>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star_half</span>
+        </td>
+    </tr>
+    <tr>
+        <td>C Language</td>
+        <td>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">grade</span>
+        </td>
+    </tr>
+    <tr>
+        <td>Python</td>
+        <td>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">grade</span>
+        </td>
+    </tr>
+    <tr>
+        <td>MySQL</td>
+        <td>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">grade</span>
+        </td>
+        </tr>
+    <tr>
+        <td>Assembly Language</td>
+        <td>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star</span>
+            <span class="material-symbols-outlined">star_half</span>
+            <span class="material-symbols-outlined">grade</span>
+        </td>
+    </tr>
+    `;
+
+    b.hidden = true;
+}
